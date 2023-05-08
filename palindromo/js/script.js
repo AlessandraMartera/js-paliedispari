d3e18eb6596bf6ea2aff3249b6723d80fc118f9d
alessandra martera <ale.martera@gmail.com>
2023-05-08 18:01:57 +0200

    verify button

--- palindromo/js/script.js
+++ palindromo/js/script.js
@@ -1,23 +1,38 @@
 // Palidroma
 
 // Chiedere all’utente di inserire una parola
-const word = prompt ("inserisci una parola");
+const word = document.getElementById("user_word").value;
 console.log(word);
 
-const invertWord = myIvertWord (word);
+const verify = documen
+
+const invertWord = myIvertWord(word);
 console.log(invertWord);
 // FUNZIONI
 // Creare una funzione per capire invertire le lettere della parola 
-function myIvertWord (str) {
+function myIvertWord(str) {
    
     let rst = str.split("");
     rst = rst.reverse();
     rst = rst.join("");
     return rst
 }
+
+
 // se la parola inserita è palindroma
 if ( word === invertWord) {
     console.log( "la parola è palindorma ");
 } else {
     console.log("la parola non è palindroma");
 } 
+
+
+// word = "ciao"
+// let invertedWord;
+// for(let i = word.length; i >= 0; i--) {
+//     let lettera = word[i]
+//     invertWord += lettera
+// }
+
+
+// console.log(invertWord) 
